92c31af11a2fa4bf8b9e5ab6f7dd474550e79dd8
Justyn Lantz <68471166+jtlantz@users.noreply.github.com>
2022-07-14 00:41:14 +0700

    Translator service and weather decoder working all good now

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -6,6 +6,8 @@ import 'dart:convert';
 
 import '../model/weather.dart';
 import '../model/fish.dart';
+import 'translator.dart';
+
 // https://stackoverflow.com/questions/53299447/flutter-http-headers
 Map<String, String> requestHeaders = {
   'api-key': ApiConstants.apiKeyData,
@@ -33,17 +35,55 @@ Future<Fish> getTodayFish() async {
   return res.first;
 }
 
+Future getLatLong(String city) async {
 
-Future getCurrentWeather() async {
-  String city = "Bangkok";
-  String apiKey = "asdasdasd";
-  var url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric");
+  String googleMapsApiKey = "AIzaSyBsW-yDLoPwRD0A9rViUcQ3zoeKD4bQ2MU";
+  var url = Uri.parse('https://maps.googleapis.com/maps/api/geocode/json?address=$city&key=$googleMapsApiKey');
+  var response = await http.get(url);
+  var jsonData = json.decode(response.body);
+  var lat = jsonData['results'][0]['geometry']['location']['lat'];
+  var lon = jsonData['results'][0]['geometry']['location']['lng'];
+  return {'lat': lat, 'lon': lon};
+}
+
+
+Future<Weather> getCurrentWeather(cityInThai) async {
+  var translated = await translateEnglish(cityInThai);
+  var geolocation = await getLatLong(translated);
+  var lat = geolocation['lat'];
+  var lon = geolocation['lon'];
+
+  String openWeatherApiKey = "a19222ec79bed7c6b9424d174a282b91";
+  var url = Uri.parse("https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&appid=$openWeatherApiKey");
 
   final response = await http.get(url);
 
+  var desc = await translateThai(jsonDecode(response.body)['current']['weather'][0]['description']);
   if (response.statusCode == 200) {
-    return Weather.fromJson(jsonDecode(response.body));
-  } else{
-    return Weather(temp: -999,feelsLike: -999, low: -999, high: -999, description: 'ไม่พบข้อมูลการตรวจสภาพอากาศ, กรุณาลองพิมเป็นภาษาอังกฦษ');
+    return Weather.fromJson(jsonDecode(response.body), desc);
+  } else {
+    return Weather(lat:0,
+        lon:0,
+        temp: 0,
+        feelsLike: 0,
+        low: 0,
+        high: 0,
+        description: 'ไม่พบข้อมูลการตรวจสภาพอากาศ, กรุณาลองพิมเป็นภาษาอังกฦษ',
+        icon: Uri.parse('https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png')
+    );
   }
+}
+
+//หนองปรือ
+// ชลบุรี
+// พัทยา
+// สมุทรปราการ
+
+Future<void> main() async {
+  var x = await getCurrentWeather('ศาลายา');
+  print(x.toString());
+  // print(getCurrentWeather('หนองปรือ'));
+  // print(getCurrentWeather('ชลบุรี'));
+  // print(getCurrentWeather('พัทยา'));
+  // print(getCurrentWeather('สมุทรปราการ'));
 }
---
+++ lib/services/translator.dart
@@ -0,0 +1,23 @@
+import 'package:fisher/components/constant.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:translator/translator.dart';
+
+/**
+ * The steps are as follows
+ * 1. User inputs name of city in thai
+ * 2. We translate the city name to english
+ * 3. We get the lat/long of the city from
+ */
+
+Future translateEnglish(input) async {
+  var translator = GoogleTranslator();
+  var result = await translator.translate(input, from: 'th', to: 'en');
+  return result.text;
+}
+
+Future translateThai(input) async {
+  var translator = GoogleTranslator();
+  var result = await translator.translate(input, from: 'en', to: 'th');
+  return result.text;
+}
